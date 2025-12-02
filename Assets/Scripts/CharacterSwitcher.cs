b905a866242dc700a9a407e67d8e497921be30f3
SinanEm <crktemr@gmail.com>
2025-12-02 16:55:54 +0300

    Update CharacterSwitcher.cs

--- Assets/Scripts/CharacterSwitcher.cs
+++ Assets/Scripts/CharacterSwitcher.cs
@@ -1,21 +1,18 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
 public class CharacterSwitcher : MonoBehaviour
 {
-    public GameObject Character;      
-    public GameObject Character2;     
+    public GameObject Character;
+    public GameObject Character2;
 
     private GameObject currentCharacter;
     private GameObject inactiveCharacter;
 
-    private bool canSwitch = true;   
-
-    public float firstCharacterTime = 10f; 
+    private bool canSwitch = true;
     void Start()
     {
-       
-        if (Character.activeSelf)
+       if (Character.activeSelf)
         {
             currentCharacter = Character;
             inactiveCharacter = Character2;
@@ -27,57 +24,45 @@ public class CharacterSwitcher : MonoBehaviour
         }
     }
 
-    void Update()
+   void Update()
     {
-        // Manuel deðiþtirme sadece kilit açýkken
         if (Input.GetKeyDown(KeyCode.Z) && canSwitch)
         {
-            SwitchToCharacter(Character); // Ýlk karakteri seçmek istiyorsan çaðýrýlýr
+            SwitchCharacter();
+            StartCoroutine(SwitchCooldown());
         }
     }
 
-    // Belirli karakteri aktif yap
-    public void SwitchToCharacter(GameObject newCharacter)
+    void SwitchCharacter()
     {
-        if (!canSwitch) return; // Kilitliyse geçiþ yapma
-
-        canSwitch = false; // Kilitle
-
-        // Pozisyon ve yönü al
         Vector3 lastPosition = currentCharacter.transform.position;
         Vector3 lastScale = currentCharacter.transform.localScale;
 
-        // Eski karakteri kapat
         currentCharacter.SetActive(false);
 
-        // Yeni karakteri aç ve pozisyona taþý
-        newCharacter.transform.position = lastPosition;
-        newCharacter.transform.localScale = lastScale;
-        newCharacter.SetActive(true);
+        inactiveCharacter.transform.position = lastPosition;
+        inactiveCharacter.transform.localScale= lastScale;
 
-        // Aktif / pasif deðiþtir
-        inactiveCharacter = currentCharacter;
-        currentCharacter = newCharacter;
+        inactiveCharacter.SetActive(true);
 
-        // Eðer aktif olan ilk karakterse süre baþlat
-        if (currentCharacter == Character)
-        {
-            StartCoroutine(FirstCharacterTimer());
-        }
-        else
-        {
-            canSwitch = true; // Süresiz karakter ise kilidi aç
-        }
+        GameObject temp = currentCharacter;
+        currentCharacter = inactiveCharacter;
+        inactiveCharacter = temp;
+
     }
 
-    // Ýlk karakterin süre limiti
-    IEnumerator FirstCharacterTimer()
+    IEnumerator SwitchCooldown()
     {
-        yield return new WaitForSeconds(firstCharacterTime);
 
-        // Süre dolunca otomatik olarak diðer karakteri aç
-        SwitchToCharacter(Character2);
-        canSwitch = true; // Artýk tekrar deðiþtirilebilir
+        canSwitch = false;
+        yield return new WaitForSeconds(5f);
+        canSwitch = true;
     }
-}
 
+
+
+
+
+
+
+}
